445f0dbd660a4f8b75ec39fb16ed409fefa84317
Fillipe Rosini <hi@frosini.dev>
2023-11-27 10:26:29 -0300

    Small fixes

--- go/cmd/day1.go
+++ go/cmd/day1.go
@@ -14,29 +14,27 @@ func init() {
 var day1Cmd = &cobra.Command{
 	Use:   "day1 [input path] [part]",
 	Short: "Day 1 solution",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE: func(_ *cobra.Command, args []string) error {
 		input, err := readInput(args[0])
 		if err != nil {
 			return err
 		}
 
 		part := args[1]
-		if part == "1" {
+		switch part {
+		case "1":
 			result, err := day1.Part1(input)
 			if err != nil {
 				return err
 			}
-
 			fmt.Printf("Day 1 part 1: %s\n", result)
-		} else if part == "2" {
-
+		case "2":
 			result, err := day1.Part2(input)
 			if err != nil {
 				return err
 			}
-
 			fmt.Printf("Day 1 part 2: %s\n", result)
-		} else {
+		default:
 			return fmt.Errorf("invalid part: %s", part)
 		}
 
