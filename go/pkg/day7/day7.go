e9d9701aa8a320dca345a3531bf25871e0c34f2f
Fillipe Rosini <frosini@slb.com>
2023-12-07 19:13:55 -0300

    Day 7 in Go

--- go/pkg/day7/day7.go
+++ go/pkg/day7/day7.go
@@ -1,43 +1,240 @@
 package day7
 
-import "bufio"
+import (
+	"bufio"
+	"errors"
+	"sort"
+	"strconv"
+	"strings"
+)
 
-var labelOrders map[rune]int
+type HandType int64
 
-func init() {
-	labelOrders = map[rune]int{
-		'A': 0,
-		'K': 1,
-		'Q': 2,
-		'J': 3,
-		'T': 4,
-		'9': 5,
-		'8': 6,
-		'7': 7,
-		'6': 8,
-		'5': 9,
-		'4': 10,
-		'3': 11,
+const (
+	HighCard HandType = iota
+	OnePair
+	TwoPair
+	ThreeOfAKind
+	FullHouse
+	FourOfAKind
+	FiveOfAKind
+)
+
+type Card rune
+
+var labelOrders map[Card]int = map[Card]int{
+	'A': 0,
+	'K': 1,
+	'Q': 2,
+	'J': 3,
+	'T': 4,
+	'9': 5,
+	'8': 6,
+	'7': 7,
+	'6': 8,
+	'5': 9,
+	'4': 10,
+	'3': 11,
+	'2': 12,
+}
+
+var jokerLabelOrders map[Card]int = map[Card]int{
+	'A': 0,
+	'K': 1,
+	'Q': 2,
+	'T': 3,
+	'9': 4,
+	'8': 5,
+	'7': 6,
+	'6': 7,
+	'5': 8,
+	'4': 9,
+	'3': 10,
+	'2': 11,
+	'J': 12,
+}
+
+func (c Card) LabelOrder() int {
+	return labelOrders[c]
+}
+
+func (c Card) JokerLabelOrder() int {
+	return jokerLabelOrders[c]
+}
+
+type Hand []Card
+
+func (h Hand) Type() HandType {
+	frequencies := make(map[Card]int)
+	var highestCount int
+	for _, c := range h {
+		frequencies[c] = frequencies[c] + 1
+		if frequencies[c] > highestCount {
+			highestCount = frequencies[c]
+		}
+	}
+
+	switch len(frequencies) {
+	case 1:
+		return FiveOfAKind
+	case 2:
+		if highestCount == 4 {
+			return FourOfAKind
+		}
+		return FullHouse
+	case 3:
+		if highestCount == 3 {
+			return ThreeOfAKind
+		}
+		return TwoPair
+	case 4:
+		return OnePair
+	default:
+		return HighCard
 	}
 }
 
-type Card struct {
-	Label rune
+func (h Hand) HighestType() HandType {
+	frequencyMap := make(map[Card]int)
+	for _, c := range h {
+		frequencyMap[c] = frequencyMap[c] + 1
+	}
+
+	sortedFrequencies := make([]int, 0)
+	for k, v := range frequencyMap {
+		if k == 'J' {
+			continue
+		}
+
+		sortedFrequencies = append(sortedFrequencies, v)
+	}
+
+	sort.Slice(sortedFrequencies, func(i, j int) bool {
+		x := sortedFrequencies[i]
+		y := sortedFrequencies[j]
+		return y < x
+	})
+
+	jokerFrequency := frequencyMap['J']
+
+	if len(frequencyMap) == 1 || sortedFrequencies[0]+jokerFrequency == 5 {
+		return FiveOfAKind
+	}
+	if sortedFrequencies[0] == 4 || sortedFrequencies[0]+jokerFrequency == 4 {
+		return FourOfAKind
+	}
+	if len(frequencyMap) == 2 || sortedFrequencies[0]+sortedFrequencies[1]+jokerFrequency == 5 {
+		return FullHouse
+	}
+	if sortedFrequencies[0] == 3 || sortedFrequencies[0]+jokerFrequency == 3 {
+		return ThreeOfAKind
+	}
+	if sortedFrequencies[0] == 2 && sortedFrequencies[1] == 2 {
+		return TwoPair
+	}
+	if sortedFrequencies[0] == 2 || jokerFrequency > 0 {
+		return OnePair
+	}
+
+	return HighCard
 }
 
-func (c *Card) LabelOrder() int {
-	return labelOrders[c.Label]
+type Bid struct {
+	Hand  Hand
+	Value uint64
 }
 
-type Hand struct {
-	Cards []*Card
-	Bid   int
+type Bids []Bid
+
+func (b Bids) Sort(handTypeFunc func(Hand) HandType, labelOrderFunc func(c Card) int) {
+	sort.Slice(b, func(i, j int) bool {
+		x := b[i]
+		y := b[j]
+
+		xType := handTypeFunc(x.Hand)
+		yType := handTypeFunc(y.Hand)
+
+		if xType == yType {
+			for i := 0; i < len(x.Hand); i++ {
+				xOrder := labelOrderFunc(x.Hand[i])
+				yOrder := labelOrderFunc(y.Hand[i])
+
+				if xOrder == yOrder {
+					continue
+				}
+
+				return xOrder > yOrder
+			}
+		}
+
+		return xType < yType
+	})
 }
 
 func Part1(scanner *bufio.Scanner) (string, error) {
-	return "", nil
+	bids, err := parseBids(scanner)
+	if err != nil {
+		return "", err
+	}
+
+	bids.Sort(func(h Hand) HandType { return h.Type() }, func(c Card) int { return c.LabelOrder() })
+	var sum uint64
+	for i, b := range bids {
+		sum += uint64(i+1) * b.Value
+	}
+
+	result := strconv.FormatUint(sum, 10)
+
+	return result, nil
+}
+
+func parseBids(scanner *bufio.Scanner) (Bids, error) {
+	bids := make([]Bid, 0)
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		line := scanner.Text()
+		rawCards, rawBid, found := strings.Cut(line, " ")
+		if !found {
+			return nil, errors.New("could not find ' ' in line")
+		}
+
+		cards := make([]Card, len(rawCards))
+		for i, c := range rawCards {
+			cards[i] = Card(c)
+		}
+
+		value, err := strconv.Atoi(rawBid)
+		if err != nil {
+			return nil, err
+		}
+
+		bid := Bid{
+			Hand:  cards,
+			Value: uint64(value),
+		}
+		bids = append(bids, bid)
+	}
+
+	return Bids(bids), nil
 }
 
 func Part2(scanner *bufio.Scanner) (string, error) {
-	return "", nil
+	bids, err := parseBids(scanner)
+	if err != nil {
+		return "", err
+	}
+
+	bids.Sort(func(h Hand) HandType { return h.HighestType() }, func(c Card) int { return c.JokerLabelOrder() })
+
+	var sum uint64
+	for i, b := range bids {
+		sum += uint64(i+1) * b.Value
+	}
+
+	result := strconv.FormatUint(sum, 10)
+
+	return result, nil
 }
--- go/pkg/day7/day7_test.go
+++ go/pkg/day7/day7_test.go
@@ -1,9 +1,128 @@
 package day7
 
-import "testing"
+import (
+	"bufio"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseBids(t *testing.T) {
+	input := `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	hands, err := parseBids(scanner)
+
+	require.Nil(t, err)
+	require.Equal(t, 5, len(hands))
+	require.Equal(t, uint64(765), hands[0].Value)
+	require.Equal(t, uint64(684), hands[1].Value)
+}
+
+func TestHandType(t *testing.T) {
+	type testCase struct {
+		hand         string
+		expectedType HandType
+	}
+	testCases := []testCase{
+		{"AAAAA", FiveOfAKind},
+		{"AA8AA", FourOfAKind},
+		{"23332", FullHouse},
+		{"TTT98", ThreeOfAKind},
+		{"23432", TwoPair},
+		{"A23A4", OnePair},
+		{"23456", HighCard},
+	}
+	for _, c := range testCases {
+		name := fmt.Sprintf("%s should be type %d", c.hand, c.expectedType)
+		t.Run(name, func(t *testing.T) {
+			hand := Hand(c.hand)
+			handType := hand.Type()
+			assert.Equal(t, c.expectedType, handType)
+		})
+	}
+}
+
+func TestBidSort(t *testing.T) {
+	bids := Bids{
+		{Hand("32T3K"), 765},
+		{Hand("T55J5"), 684},
+		{Hand("KK677"), 28},
+		{Hand("KTJJT"), 220},
+		{Hand("QQQJA"), 483},
+	}
+
+	want := []string{
+		"32T3K",
+		"KTJJT",
+		"KK677",
+		"T55J5",
+		"QQQJA",
+	}
+
+	bids.Sort(func(h Hand) HandType { return h.Type() }, func(c Card) int { return c.JokerLabelOrder() })
+
+	got := make([]string, len(bids))
+	for i, b := range bids {
+		got[i] = string(b.Hand)
+	}
+
+	assert.Equal(t, want, got)
+}
 
 func TestPart1(t *testing.T) {
+	input := `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	result, err := Part1(scanner)
+
+	require.Nil(t, err)
+	assert.Equal(t, "6440", result)
+}
+
+func TestHighestType(t *testing.T) {
+	type testCase struct {
+		hand         string
+		expectedType HandType
+	}
+	testCases := []testCase{
+		{"32T3K", OnePair},
+		{"T55J5", FourOfAKind},
+		{"KK677", TwoPair},
+		{"KTJJT", FourOfAKind},
+		{"QQQJA", FourOfAKind},
+	}
+	for _, c := range testCases {
+		name := fmt.Sprintf("%s should be type %d", c.hand, c.expectedType)
+		t.Run(name, func(t *testing.T) {
+			hand := Hand(c.hand)
+			handType := hand.HighestType()
+			assert.Equal(t, c.expectedType, handType)
+		})
+	}
 }
 
 func TestPart2(t *testing.T) {
+	input := `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	result, err := Part2(scanner)
+
+	require.Nil(t, err)
+	assert.Equal(t, "5905", result)
 }
