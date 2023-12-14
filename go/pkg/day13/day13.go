94308bcec9b0d2d155e5bcf5e4939b7ccf06be24
Fillipe Rosini <hi@frosini.dev>
2023-12-13 22:47:11 -0300

    Day 13 part 1 in Go

--- go/pkg/day13/day13.go
+++ go/pkg/day13/day13.go
@@ -8,6 +8,33 @@ import (
 
 type Map [][]rune
 
+func (m Map) Rows() []string {
+	rows := make([]string, len(m))
+	for i, r := range m {
+		rows[i] = string(r)
+	}
+	return rows
+}
+
+func (m Map) Columns() []string {
+	columns := make([]string, len(m[0]))
+	for x := 0; x < len(columns); x++ {
+		var sb strings.Builder
+		for y := 0; y < len(m); y++ {
+			sb.WriteRune(m[y][x])
+		}
+		columns[x] = sb.String()
+	}
+	return columns
+}
+
+type Kind int
+
+const (
+	Row Kind = iota
+	Column
+)
+
 func Part1(scanner *bufio.Scanner) (string, error) {
 	maps, err := parseInput(scanner)
 	if err != nil {
@@ -16,19 +43,43 @@ func Part1(scanner *bufio.Scanner) (string, error) {
 
 	var sum int
 	for _, m := range maps {
-		rows := rows(m)
-		start, _, found := findReflection(rows)
-		if found {
-			sum += 100 * (start + 1)
-		} else {
-			columns := columns(m)
-			start, _, found = findReflection(columns)
-			sum += start + 1
-		}
+		sum += solve(m, 0)
 	}
 
 	result := strconv.Itoa(sum)
 	return result, nil
+}
+
+func solve(m Map, expectedDiff int) int {
+	doSolve := func(patterns []string) int {
+		var result int
+
+		for i := 1; i < len(patterns); i++ {
+			r := min(i, len(patterns)-i)
+			previousIndex, nextIndex := i-1, i
+
+			var count int
+			for delta := 0; delta < r; delta++ {
+				previous, next := patterns[previousIndex-delta], patterns[nextIndex+delta]
+				for j := 0; j < len(previous); j++ {
+					if previous[j] == next[j] {
+						count++
+					}
+				}
+			}
+
+			maxCount := r * len(patterns[0])
+			if (maxCount - count) == expectedDiff {
+				result = i
+			}
+		}
+
+		return result
+	}
+
+	rowResult := doSolve(m.Rows())
+	columnResult := doSolve(m.Columns())
+	return rowResult*100 + columnResult
 }
 
 func parseInput(scanner *bufio.Scanner) ([]Map, error) {
@@ -55,61 +106,17 @@ func parseInput(scanner *bufio.Scanner) ([]Map, error) {
 	return maps, nil
 }
 
-func parseMap(scanner *bufio.Scanner) (Map, error) {
-	m := make([][]rune, 0)
-	for scanner.Scan() {
-		if err := scanner.Err(); err != nil {
-			return nil, err
-		}
-		line := strings.TrimSpace(scanner.Text())
-		m = append(m, []rune(line))
-	}
-	return m, nil
-}
-
-func rows(m Map) []string {
-	rows := make([]string, len(m))
-	for i, r := range m {
-		rows[i] = string(r)
-	}
-	return rows
-}
-
-func findReflection(patterns []string) (start, end int, found bool) {
-	for i := 0; i < len(patterns)-1; i++ {
-		if patterns[i] == patterns[i+1] && isReflection(patterns, i, i+1) {
-			start = i
-			end = i + 1
-			found = true
-			return
-		}
-	}
-	return
-}
-
-func isReflection(patterns []string, startIndex, endIndex int) bool {
-	for startIndex >= 0 && endIndex < len(patterns) {
-		if patterns[startIndex] != patterns[endIndex] {
-			return false
-		}
-		startIndex--
-		endIndex++
-	}
-	return true
-}
-
-func columns(m Map) []string {
-	columns := make([]string, len(m[0]))
-	for x := 0; x < len(columns); x++ {
-		var sb strings.Builder
-		for y := 0; y < len(m); y++ {
-			sb.WriteRune(m[y][x])
-		}
-		columns[x] = sb.String()
-	}
-	return columns
-}
-
 func Part2(scanner *bufio.Scanner) (string, error) {
-	return "", nil
+	maps, err := parseInput(scanner)
+	if err != nil {
+		return "", nil
+	}
+
+	var sum int
+	for _, m := range maps {
+		sum += solve(m, 1)
+	}
+
+	result := strconv.Itoa(sum)
+	return result, nil
 }
--- go/pkg/day13/day13_test.go
+++ go/pkg/day13/day13_test.go
@@ -9,46 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestFindColumnReflection(t *testing.T) {
-	input := `#.##..##.
-..#.##.#.
-##......#
-##......#
-..#.##.#.
-..##..##.
-#.#.##.#.`
-	scanner := bufio.NewScanner(strings.NewReader(input))
-
-	m, err := parseMap(scanner)
-	require.Nil(t, err)
-	c := columns(m)
-	start, end, found := findReflection(c)
-
-	assert.Equal(t, true, found)
-	assert.Equal(t, 4, start)
-	assert.Equal(t, 5, end)
-}
-
-func TestFindRowReflection(t *testing.T) {
-	input := `#...##..#
-#....#..#
-..##..###
-#####.##.
-#####.##.
-..##..###
-#....#..#`
-	scanner := bufio.NewScanner(strings.NewReader(input))
-
-	m, err := parseMap(scanner)
-	require.Nil(t, err)
-	c := rows(m)
-	start, end, found := findReflection(c)
-
-	assert.Equal(t, true, found)
-	assert.Equal(t, 3, start)
-	assert.Equal(t, 4, end)
-}
-
 func TestPart1(t *testing.T) {
 	input := `#.##..##.
 ..#.##.#.
@@ -71,4 +31,28 @@ func TestPart1(t *testing.T) {
 
 	require.Nil(t, err)
 	assert.Equal(t, "405", result)
+}
+
+func TestPart2(t *testing.T) {
+	input := `#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	result, err := Part2(scanner)
+
+	require.Nil(t, err)
+	assert.Equal(t, "400", result)
 }
